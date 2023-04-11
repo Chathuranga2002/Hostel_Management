5c77acb7b10fa9e5504c4049f34be0bb66c25a6b
Chathuranga2002 <121780050+Chathuranga2002@users.noreply.github.com>
2023-04-11 20:08:14 +0530

    deshbord create

---
+++ src/hostelManagement/controller/DeashBordFormControler.java
@@ -0,0 +1,61 @@
+package hostelManagement.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class DeashBordFormControler {
+
+
+        @FXML
+        private AnchorPane Pane;
+
+        @FXML
+        private Text txttime;
+
+        @FXML
+        private Label lbltitile;
+
+        @FXML
+        private Label lblDate;
+
+        @FXML
+        private AnchorPane pane;
+
+        @FXML
+        private AnchorPane pane1;
+
+        @FXML
+        void LogoutOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void deashBordOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void deatilsOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void recevationOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void roomsOnAction(ActionEvent event) {
+
+        }
+
+        @FXML
+        void studentOnAction(ActionEvent event) {
+
+        }
+
+
+
+}
---
+++ src/hostelManagement/controller/LoginFormControler.java
@@ -0,0 +1,4 @@
+package hostelManagement.controller;
+
+public class LoginFormControler {
+}
